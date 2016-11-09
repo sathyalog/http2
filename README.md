ee81c288d7fb4a53c800686ff342a665e27f62f2
svaka1 <svakacharla@sapient.com>
2016-11-09 20:39:11 +0530

    http2 protocol implemented and also server push

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Understanding HTTP/2 from scratch.
 Please visit https://sathyalog.wordpress.com/2016/10/26/http2-from-scratch/ to understand more.
 
 Source : **https://mor10.com**
+
+
+How to Run this Project?
+
+You can start running this project with *gulp* command initially in command prompt.
+
+This gulp will minify images,css,js and copy all files to production folder.
+
+After gulp in command prompt, run *npm start* or *node server.js*
---
+++ server.js
@@ -0,0 +1,34 @@
+const port = 3000
+const spdy = require('spdy')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.setHeader('Link', '<//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700,700i,900,900i>; rel=prefetch,</style-main.css>;rel = prefetch,/CSS/header.css;rel = prefetch,<//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js>;rel = prefetch,</JS/libs/jquery.flexslider-min.js>;rel = prefetch,</images/mainpromo/welcome02-1600.jpg>;rel = prefetch');
+  next();
+});
+
+app.use(express.static('production'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+const options = {
+    key: fs.readFileSync(__dirname + '/mykey.pem'),
+    cert:  fs.readFileSync(__dirname + '/my-cert.pem')
+}
+
+spdy
+  .createServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      console.error(error)
+      return process.exit(1)
+    } else {
+      console.log('Listening on port: ' + port + '.')
+    }
+  })
